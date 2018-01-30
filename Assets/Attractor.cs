0ee5ca7cf43a3b84ad9a1c14510cc4dabaf01c7f
Bereim <35741860+Bereim@users.noreply.github.com>
2018-01-30 17:58:03 -0600

    Added Scripts

---
+++ Assets/Attractor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+	static public Vector3 POS = Vector3.zero;
+
+	public float radius = 10, xPhase = 0.5f, yPhase = 0.4f, zPhase = 0.1f;
+	
+	void FixedUpdate() {
+		Vector3 scale = transform.localScale;
+		Vector3 tPos = new Vector3( 
+			Mathf.Sin(xPhase * Time.time) * radius * scale.x,
+			Mathf.Sin(yPhase * Time.time) * radius * scale.y,
+			Mathf.Sin(zPhase * Time.time) * radius * scale.z
+		);
+		transform.position = tPos;
+		POS = tPos;
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	static public Spawner S;
+	static public List<Boid> boids;
+
+	public GameObject boidprefab;
+	public Transform boidAnchor;
+	public int numBoids = 100;
+	public float spawnRadius = 100f, spawnDelay = 0.1f;
+
+	public float velocity = 30f, neighborDist = 30f, velMatching = 0.25f, flockCentering = 0.2f;
+	public float collDist = 4f, collAvoid = 2f;
+	public float attractPull = 2f, attractPush = 2f, attractPushDist = 5f;
+
+	void Awake() {
+		S = this;
+		boids = new List<Boid>();
+		InstantiateBoid();
+	}
+
+	public void InstantiateBoid() {
+		GameObject go = Instantiate(boidprefab);
+		Boid b = go.GetComponent<Boid>();
+		b.transform.SetParent(boidAnchor);
+		boids.Add(b);
+		if(boids.Count < numBoids) {
+			Invoke("InstantiateBoid", spawnDelay);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
